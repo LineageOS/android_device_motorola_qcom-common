123796ffc04e4db0b00104ba5225440f2b60db64
Sean McCreary <mccreary@mcwest.org>
2017-08-09 12:54:57 -0600

    qcom-common: Update extract-files.sh
    Replace extract-files.sh with a version that uses the helper
    script in vendor/cm/build/tools/extract_utils.sh
    
    Change-Id: I475a4adf89a6f30f129fd880512b5be8fc885ee6

--- extract-files.sh
+++ extract-files.sh
@@ -1,80 +1,110 @@
 #!/bin/bash
-
-# Check to see if the user passed a folder in to extract from rather than adb pull
-if [ $# -eq 1 ]; then
-    COPY_FROM=$1
-    test ! -d "$COPY_FROM" && echo error reading dir "$COPY_FROM" && exit 1
-fi
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
 
-function extract() {
-    for FILE in `egrep -v '(^#|^$)' $1`; do
-        echo "Extracting /system/$FILE ..."
-        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-        DEST=${PARSING_ARRAY[1]}
-        if [ -z $DEST ]; then
-            DEST=$FILE
-        fi
-        DIR=`dirname $FILE`
-        if [ ! -d $2/$DIR ]; then
-            mkdir -p $2/$DIR
-        fi
-        if [ "$COPY_FROM" = "" ]; then
-            # Try CM target first
-            if [ -f /system/$DEST ]; then
-                adb pull /system/$DEST $2/$DEST
-            else
-                # if file does not exist try OEM target
-                if [ "$?" != "0" ]; then
-                    adb pull /system/$FILE $2/$DEST
-                fi
-            fi
-        else
-            # Try CM target first
-            if [ -f $COPY_FROM/$DEST ]; then
-                cp $COPY_FROM/$DEST $2/$DEST
-            else
-                # if file does not exist try OEM target
-                if [ "$?" != "0" ]; then
-                    cp $COPY_FROM/$FILE $2/$DEST
-                fi
-            fi
-        fi
-    done
-}
+# XXXX These are defined by the upstream wrapper.
+#      DO NOT RUN THIS SCRIPT DIRECTLY
+# DEVICE=**** FILL IN DEVICE NAME ****
+# VENDOR=*** FILL IN VENDOR ****
 
-DEVICE_BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $DEVICE_BASE/*
-
-# Extract the device specific files
-extract ../../$VENDOR/$DEVICE/device-proprietary-files.txt $DEVICE_BASE
-
-# Check if their is a family device tree for this device
-if [ ! -z $FAMILY_DEVICE ]; then
-    FAMILY_BASE=../../../vendor/$VENDOR/$FAMILY_DEVICE/proprietary
-    rm -rf $FAMILY_BASE/*
-
-    # Check if there are files common to all devices but family specific
-    if [ -f ../../$VENDOR/$FAMILY_DEVICE/proprietary-files.txt ]; then
-        extract ../../$VENDOR/$FAMILY_DEVICE/proprietary-files.txt $DEVICE_BASE
-    fi
-    # Extract the files common to all devices using this common device tree
-    extract ../../$VENDOR/$FAMILY_DEVICE/common-proprietary-files.txt $FAMILY_BASE
+if [ -z "${DEVICE}" ]; then
+    echo "DEVICE undefined, exiting"
+    exit 1
 fi
 
-# Check if their is a common device tree for this device
+if [ -z "${VENDOR}" ]; then
+    echo "VENDOR undefined, exiting"
+    exit 1
+fi
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# default to not sanitizing the vendor folder before extraction
+clean_vendor=false
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --path )           shift
+                                SRC=$1
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                clean_vendor=false
+                                ;;
+        -c | --clean-vendor )   clean_vendor=true
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+# Check if there is a common device tree for this device
 if [ ! -z $COMMON_DEVICE ]; then
-    COMMON_BASE=../../../vendor/$VENDOR/$COMMON_DEVICE/proprietary
-    rm -rf $COMMON_BASE/*
-
-    # Check if there are files common to all devices but device specific
-    if [ -f ../../$VENDOR/$COMMON_DEVICE/proprietary-files.txt ]; then
-        extract ../../$VENDOR/$COMMON_DEVICE/proprietary-files.txt $DEVICE_BASE
-    fi
+    # Initialize the helper for common device
+    setup_vendor "$COMMON_DEVICE" "$VENDOR" "$CM_ROOT" true $clean_vendor
     # Extract the files common to all devices using this common device tree
-    extract ../../$VENDOR/$COMMON_DEVICE/common-proprietary-files.txt $COMMON_BASE
+    extract ../../$VENDOR/$COMMON_DEVICE/common-proprietary-files.txt "$SRC" "$SECTION"
 fi
 
-../$COMMON_DEVICE/setup-makefiles.sh
+# Check if there is a family device tree for this device
+if [ ! -z $FAMILY_DEVICE ]; then
+    # Initialize the helper for family
+    setup_vendor "$FAMILY_DEVICE" "$VENDOR" "$CM_ROOT" true $clean_vendor
+    # Extract the files common to all devices using this common device tree
+    extract ../../$VENDOR/$FAMILY_DEVICE/common-proprietary-files.txt "$SRC" "$SECTION"
+fi
+
+# Check if there are files common to all devices but device specific
+if [ ! -z $COMMON_DEVICE -a -f ../../$VENDOR/$COMMON_DEVICE/proprietary-files.txt ]
+then
+    # Initialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false $clean_vendor
+    extract ../../$VENDOR/$COMMON_DEVICE/proprietary-files.txt "$SRC" "$SECTION"
+    clean_vendor=false
+fi
+
+# Check if there are files common to all devices but family specific
+if [ ! -z $FAMILY_DEVICE -a -f ../../$VENDOR/$FAMILY_DEVICE/proprietary-files.txt ]
+then
+    # (Re)initialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false $clean_vendor
+    extract ../../$VENDOR/$FAMILY_DEVICE/proprietary-files.txt "$SRC" "$SECTION"
+    clean_vendor=false
+fi
+
+# (Re)initialize the helper for device
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false $clean_vendor
+# Extract the device specific files
+extract ../../$VENDOR/$DEVICE/device-proprietary-files.txt "$SRC" "$SECTION"
+
+"$MY_DIR"/setup-makefiles.sh
